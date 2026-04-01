d5a68104900c353368f6d6269eef924a0e7c019d
senbet22 <senbet22@users.noreply.github.com>
2026-04-01 09:49:41 +0200

    Created Videos Model: based on what frontend expects.

---
+++ Backend/Model/Videos.cs
@@ -0,0 +1,10 @@
+public class Videos
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required string VideoURL { get; set; }
+    public int NumberOfLikes { get; set; }
+
+}
+
