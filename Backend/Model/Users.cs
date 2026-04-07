abb603cecb84243b85ce167e3aee38d81d471e62
senbet22 <senbet22@users.noreply.github.com>
2026-04-07 12:38:28 +0200

    Updated .NET from Net 8.0 to 10.0

---
+++ Backend/Model/Users.cs
@@ -0,0 +1,8 @@
+public class Users
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public int NumberOfVideosUploaded { get; set; }
+}
